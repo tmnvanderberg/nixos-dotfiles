84159a8ebe7334d81b44c9f97622d5cd27547048
Timon van der Berg <tmnvanderberg@gmail.com>
2022-05-06 23:59:02 +0200

    add bat

--- system/configuration.nix
+++ system/configuration.nix
@@ -1,10 +1,19 @@
 { config, pkgs, ... }:
 
+let 
+  my-python-packages = python-packages: with python-packages; [
+    pyx
+  ];
+  python-with-my-packages = pkgs.python3.withPackages my-python-packages;
+in
 {
   imports =
     [ 
       ./hardware-configuration.nix
     ];
+
+  # use VIM
+  programs.vim.defaultEditor = true;
 
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
@@ -13,8 +22,10 @@
   # Probe windows
   boot.loader.grub.useOSProber = true;
 
+  # hostname
   networking.hostName = "nixrp200";
 
+  # localization
   time.timeZone = "Europe/Amsterdam";
 
   # The global useDHCP flag is deprecated, therefore explicitly set to false here.
@@ -43,23 +54,39 @@
   services.xserver.layout = "us";
   services.xserver.xkbOptions = "eurosign:e";
 
+  # sound & bt
   sound.enable = true;
   hardware.pulseaudio.enable = true;
   nixpkgs.config.pulseaudio = true;
   hardware.bluetooth.enable = true;
+
+  virtualisation.docker.enable = true;
   
+  # set up user & groups
   users.users.tmn = {
     isNormalUser = true;
     home = "/home/tmn";
     description = "Timon van der Berg";
-    extraGroups = [ "wheel" "networkmanager" "audio" "video" "input" "tty"];
+    extraGroups = [ 
+      "wheel" 
+      "networkmanager" 
+      "audio" 
+      "video" 
+      "input" 
+      "tty"
+    ];
     initialHashedPassword = "$6$TwDO8O6vF6gpV/OC$wOAFoqvpXV9WnbTvbqLRmQlGcb8oNMJeIMoyV1RtdLJztCZGZD3M0tNb6piyKSnoAz5UfVPwOAsjIB3SG8gE9/";
   };
 
+  # enable unfree packages like propriatary drivers
   nixpkgs.config.allowUnfree = true;
+
   environment.systemPackages = with pkgs; [
-    vim
+    # Use _configurable because it comes with python support
+    vim_configurable
     wget
+    ark
+    tmux
     firefox
     git
     tdesktop
@@ -83,8 +110,15 @@
     silver-searcher
     nixfmt
     python
+    python-with-my-packages
     signal-desktop
     ctags
+    tigervnc
+    docker
+    clojure
+    boot
+    lua
+    bat
   ];
 
   services.openssh.enable = true;
