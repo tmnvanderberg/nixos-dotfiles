b3ffcaa8e94ceb4a006635bd826bd6c19e465e1f
Timon van der Berg <tmnvanderberg@gmail.com>
2024-03-05 13:08:46 +0100

    use amd driver
    Signed-off-by: Timon van der Berg <tmnvanderberg@gmail.com>

--- nixr200p/configuration.nix
+++ nixr200p/configuration.nix
@@ -20,6 +20,11 @@ in
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  # use amd driver for GPU
+  boot.initrd.kernelModules = [ "amdgpu" ];
+  systemd.tmpfiles.rules = [
+    "L+    /opt/rocm/hip   -    -    -     -    ${pkgs.rocmPackages.clr}"
+  ];
   # hostname
   networking.hostName = "nixrp200";
 
@@ -104,35 +109,6 @@ in
 
   # enable unfree packages like propriatary drivers
   nixpkgs.config.allowUnfree = true;
-
-  # nvidia gpu drivers
-  services.xserver.videoDrivers = [ "nvidia" ];
-  hardware.nvidia = {
-    # Modesetting is required.
-    modesetting.enable = true;
-
-    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-    powerManagement.enable = false;
-    # Fine-grained power management. Turns off GPU when not in use.
-    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-    powerManagement.finegrained = false;
-
-    # Use the NVidia open source kernel module (not to be confused with the
-    # independent third-party "nouveau" open source driver).
-    # Support is limited to the Turing and later architectures. Full list of 
-    # supported GPUs is at: 
-    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
-    # Only available from driver 515.43.04+
-    # Do not disable this unless your GPU is unsupported or if you have a good reason to.
-    open = false;
-
-    # Enable the Nvidia settings menu,
-    # accessible via `nvidia-settings`.
-    nvidiaSettings = true;
-
-    # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
-  };
 
   # Enable OpenGL
   hardware.opengl = {
