9fd5406acfbfad9a4c2f0c4dbfe7c34af1162079
Timon van der Berg <tmnvanderberg@gmail.com>
2024-03-05 13:12:16 +0100

    add tailscale service
    Signed-off-by: Timon van der Berg <tmnvanderberg@gmail.com>

--- nixr200p/configuration.nix
+++ nixr200p/configuration.nix
@@ -47,6 +47,34 @@ in
   networking.interfaces.eno1.useDHCP = true;
   networking.interfaces.wlp7s0.useDHCP = true;
 
+  services.tailscale.enable = true;
+
+  systemd.services.tailscale-autoconnect = {
+    description = "Automatic connection to Tailscale";
+
+    # make sure tailscale is running before trying to connect to tailscale
+    after = [ "network-pre.target" "tailscale.service" ];
+    wants = [ "network-pre.target" "tailscale.service" ];
+    wantedBy = [ "multi-user.target" ];
+
+    # set this service as a oneshot job
+    serviceConfig.Type = "oneshot";
+
+    # have the job run this shell script
+    script = with pkgs; ''
+      # wait for tailscaled to settle
+      sleep 2
+
+      # check if we are already authenticated to tailscale
+      status="$(${tailscale}/bin/tailscale status -json | ${jq}/bin/jq -r .BackendState)"
+      if [ $status = "Running" ]; then # if so, then do nothing
+        exit 0
+      fi
+
+      # otherwise authenticate with tailscale
+      ${tailscale}/bin/tailscale up -authkey   <REDACTED>  '';
+  };
+
   # Select internationalisation properties.
   i18n.supportedLocales = [ "all" "en_US.UTF-8" ];
   i18n.defaultLocale = "en_US.UTF-8";
